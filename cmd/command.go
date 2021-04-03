6ad1ad23eaf2e88f5cfece31ac5a1409d969af02
xuthus5 <xuthus5@gmail.com>
2021-04-03 22:44:16 +0800

    hide: post cmd

--- cmd/command.go
+++ cmd/command.go
@@ -1,16 +1,11 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/guonaihong/gout"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 	"pastebin/common"
 	"pastebin/server"
-	"pastebin/server/driver"
-
-	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -23,30 +18,62 @@ var (
 	}
 
 	cmdPost = &cobra.Command{
-		Use:   "post [...]",
+		Use:   "post",
 		Short: "Post code segment to https://xpb.xuthus.cc",
-		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			for _, body := range args {
-				var cs driver.CodeSegmentRecord
-				if err := json.Unmarshal([]byte(body), &cs); err != nil {
-					log.Errorf("post body: %v\nget err: %v\nskip...", body, err)
-					continue
-				}
-
-				if cs.Title == "" || cs.Content == "" {
-					log.Errorf("post title or content empty")
-					continue
-				}
-
-				var pb server.Response
-				if err := gout.POST("").Debug(true).SetBody(cs).BindJSON(&pb).Do(); err != nil {
-					log.Errorf("post err: %v", err)
-					continue
-				}
-
-				_, _ = fmt.Fprintf(os.Stdout, "url: https://cs.xuthus.cc/%v", pb.Data.(driver.CodeSegmentRecord).ShortKey)
+		Args: func(cmd *cobra.Command, args []string) error {
+			_, err := cmd.Flags().GetStringSlice("post")
+			if err != nil {
+				log.Errorf("get flags err: %+v", err)
+				return err
 			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			//var cs driver.CodeSegmentRecord
+			//err := json.Unmarshal([]byte(record), &cs)
+			//if err != nil {
+			//	log.Errorf("err: %v", err)
+			//	return
+			//}
+			//
+			//if cs.Title == "" || cs.Content == "" {
+			//	log.Errorf("post title or content empty")
+			//	return
+			//}
+			//
+			//var pb server.Response
+			//if err := gout.POST("").Debug(true).SetBody(cs).BindJSON(&pb).Do(); err != nil {
+			//	log.Errorf("post err: %v", err)
+			//	return
+			//}
+			//
+			//_, _ = fmt.Fprintf(os.Stdout, "url: https://cs.xuthus.cc/%v", pb.Data.(driver.CodeSegmentRecord).ShortKey)
+			//params, _ := cmd.Flags().GetStringSlice("post")
+			//
+			//for i, param := range params {
+			//	log.Infof("idx: %v, param: %+v", i, param)
+			//}
+
+			//for _, body := range params {
+			//	var cs driver.CodeSegmentRecord
+			//	if err := json.Unmarshal([]byte(body), &cs); err != nil {
+			//		log.Errorf("post body: %v\nget err: %v\nskip...", body, err)
+			//		continue
+			//	}
+			//
+			//	if cs.Title == "" || cs.Content == "" {
+			//		log.Errorf("post title or content empty")
+			//		continue
+			//	}
+			//
+			//	var pb server.Response
+			//	if err := gout.POST("").Debug(true).SetBody(cs).BindJSON(&pb).Do(); err != nil {
+			//		log.Errorf("post err: %v", err)
+			//		continue
+			//	}
+			//
+			//	_, _ = fmt.Fprintf(os.Stdout, "url: https://cs.xuthus.cc/%v", pb.Data.(driver.CodeSegmentRecord).ShortKey)
+			//}
 		},
 	}
 
